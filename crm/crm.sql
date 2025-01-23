c4ed8977fde04d290148ed37347d11c44e627e3a
Nighat Ansari <nighat.ansari@kellogg.northwestern.edu>
2025-01-23 02:36:27 +0000

    case study group work

--- crm/crm.sql
+++ crm/crm.sql
@@ -2,4 +2,42 @@
 -- run of this script
 -- e.g. DROP TABLE IF EXISTS ______;
 
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS activity;
+DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS salesperson;
+
 -- CREATE TABLES
+CREATE TABLE CONTACTS (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    email TEXT,
+    address TEXT,
+    phone TEXT,
+    company_id INT
+);
+
+CREATE TABLE ACTIVITY (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    activity_type TEXT,
+    date_time date,
+    notes TEXT,
+    contact_id INT,
+    salesperson_id INT
+);
+CREATE TABLE COMPANIES (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT
+);
+
+CREATE TABLE SALESPERSON (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    company_name TEXT,
+    first_name TEXT,
+    last_name TEXT,
+    title TEXT,
+    email TEXT,
+    phone TEXT
+);
